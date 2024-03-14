5005498ce6fdb5f23c55920f98c09a3332ded8d3
scannerkkk <2436587369@qq.com>
2024-03-14 19:06:47 +0800

    update 3.14

---
+++ src/timerManage.cpp
@@ -0,0 +1,129 @@
+#include "timerManage.h"
+
+TimerManage::TimerManage() {
+    heap_.resize(64);
+}
+
+TimerManage::~TimerManage() {
+    clear();
+}
+
+void TimerManage::swapNode_(size_t i,size_t j) {
+    assert(i >= 0 && i < heap_.size());
+    assert(j >= 0 && j < heap_.size());
+    swap(heap_[i],heap_[j]);
+    ref_[heap_[i].id] = j;
+    ref_[heap_[j].id] = i;
+}
+
+void TimerManage::siftUp_(size_t child) {
+    assert(child >= 0 && child < heap_.size());
+    size_t parent = (child - 1) >> 1;
+    while (child > 0) {
+        if (heap_[parent] > heap_[child]) {
+            swapNode_(child,parent);
+            child = parent;
+            parent = (child - 1) >> 1;
+        } else {
+            break;
+        }
+    }
+}
+
+bool TimerManage::siftDown_(size_t parent,size_t n) {
+    int child = (parent << 1) | 1,idx = parent;
+    while (child < n) {
+        if (child + 1 < n && heap_[child] > heap_[child + 1]) {
+            child ++;
+        }
+        if (heap_[child] < heap_[parent]) {
+            swapNode_(idx,child);
+            idx = child;
+            child = (child << 1) | 1;
+        }
+        break;
+    }
+    return idx > parent;
+}
+
+void TimerManage::del_(size_t index) {
+    assert(index >= 0 && index < heap_.size());
+    size_t n = heap_.size() - 1;
+    if (index < n) {
+        swapNode_(index,n);
+        if (!siftDown_(index,n)) {
+            siftUp_(index);
+        }
+    }
+    ref_.erase(heap_.back().id);
+    heap_.pop_back();
+}
+
+void TimerManage::adjust(int id,int newExpires) {
+    assert(!heap_.empty() && ref_.count(id));
+    heap_[ref_[id]].expires = clock::now() + ms(newExpires);
+    siftDown_(ref_[id],heap_.size());
+}
+
+void TimerManage::add(int id,int timeOut,const timeoutCallBack& cb) {
+    assert(id >= 0);
+    if (ref_.count(id)) {
+        int tmp = ref_[id];
+        heap_[tmp].expires = clock::now() + ms(timeOut);
+        heap_[tmp].cb = cb;
+        if (!siftDown_(tmp,heap_.size())) {
+            siftUp_(tmp);
+        }
+    } else {
+        size_t n = heap_.size();
+        ref_[id] = n;
+        heap_.push_back({id,clock::now() + ms(timeOut),cb});
+        siftUp_(n);
+    }
+}
+
+void TimerManage::doWork(int id) {
+    if (heap_.empty() || !ref_.count(id)) {
+        return;
+    }
+    size_t i = ref_[id];
+    auto node = heap_[i];
+    node.cb();
+    del_(i);
+}
+
+void TimerManage::tick() {
+    if (heap_.empty()) {
+        return;
+    }
+    while (!heap_.empty()) {
+        TimerNode node = heap_.front();
+        if (std::chrono::duration_cast<ms>(node.expires - clock::now()).count() > 0) {
+            break;
+        }
+        node.cb();
+        pop();
+    }
+}
+
+void TimerManage::pop() {
+    assert(!heap_.empty());
+    del_(0);
+}
+
+void TimerManage::clear() {
+    ref_.clear();
+    heap_.clear();
+}
+
+int TimerManage::getNextTick() {
+    tick();
+    size_t res = -1;
+    if (!heap_.empty()) {
+        res = std::chrono::duration_cast<ms>(heap_.front().expires - clock::now()).count();
+        if (res < 0) {
+            res = 0;
+        }
+    }
+    return res;
+}
---
+++ src/timerManage.h
@@ -0,0 +1,53 @@
+#ifndef TIMER_MANAGE_H
+#define TIMER_MANAGE_H
+
+#include <queue>
+#include <unordered_map>
+#include <ctime>
+#include <algorithm>
+#include <arpa/inet.h>
+#include <functional>
+#include <cassert>
+#include <chrono>
+#include "log.h"
+
+typedef std::function<void()> timeoutCallBack;
+typedef std::chrono::high_resolution_clock clock;
+typedef std::chrono::milliseconds ms;
+typedef clock::time_point timeStamp;
+
+struct TimerNode {
+    int id;
+    timeStamp expires;
+    timeoutCallBack cb;
+    bool operator< (const TimerNode& other) const {
+        return expires < other.expires;
+    }
+    bool operator> (const TimerNode& other) const {
+        return expires > other.expires;
+    }
+};
+
+class TimerManage {
+public:
+    TimerManage();
+    ~TimerManage();
+
+    void adjust(int,int);
+    void add(int,int,const timeoutCallBack&);
+    void doWork(int);
+    void clear();
+    void tick();
+    void pop();
+    int getNextTick();
+
+private:
+    void del_(size_t);
+    void siftUp_(size_t);
+    bool siftDown_(size_t,size_t);
+    void swapNode_(size_t,size_t);
+
+    std::vector<TimerNode> heap_;
+    std::unordered_map<int,size_t> ref_;
+};
+#endif
